e1e2ed217ea85da7ad9f25dd79c57d66ce600289
nov matake <nov@matake.jp>
2010-04-24 02:56:08 +0900

    remove this test for now

--- spec/fb_graph/user_spec.rb
+++ spec/fb_graph/user_spec.rb
@@ -5,11 +5,3 @@ describe FbGraph::User, '.me' do
     FbGraph::User.me('access_token').should == FbGraph::User.new('me', :access_token => 'access_token')
   end
 end
-
-describe FbGraph::User, '#profile' do
-  it 'should setup profile attributes' do
-    # TODO
-    user = FbGraph::User.new('matake').profile
-    puts user.inspect
-  end
-end
