769814dfdbc801d0dee7aa7e47541618e63d3f1d
nov matake <nov@matake.jp>
2010-09-06 23:52:41 +0900

    documentation for how to update page's wall

--- lib/fb_graph/connections/feed.rb
+++ lib/fb_graph/connections/feed.rb
@@ -1,5 +1,6 @@
 module FbGraph
   module Connections
+    # = Page (wall post)
     module Feed
       def feed(options = {})
         posts = self.connection(:feed, options)
@@ -10,6 +11,28 @@ module FbGraph
         end
       end
 
+      # == Update User Wall
+      # 
+      # me = FbGraph::User.me(ACCESS_TOKEN)
+      # me.feed!(
+      #   :message => 'Updating via FbGraph',
+      #   :picture => 'https://graph.facebook.com/matake/picture',
+      #   :link => 'http://github.com/nov/fb_graph',
+      #   :name => 'FbGraph',
+      #   :description => 'A Ruby wrapper for Facebook Graph API'
+      # )
+      # 
+      # == Update Page Wall
+      # <em>NOTE: you need the page admin's access_token</em>
+      # 
+      # page = FbGraph::Page.new(page_id, :access_token => ACCESS_TOKEN)
+      # page.feed!(
+      #   :message => 'Updating via FbGraph',
+      #   :picture => 'https://graph.facebook.com/matake/picture',
+      #   :link => 'http://github.com/nov/fb_graph',
+      #   :name => 'FbGraph',
+      #   :description => 'A Ruby wrapper for Facebook Graph API'
+      # )
       def feed!(options = {})
         post = post(options.merge(:connection => 'feed'))
         Post.new(post.delete(:id), options.merge(post).merge(
