2f13527e5ed9bef147057cf9d03d143731b91e71
nov matake <nov@matake.jp>
2010-09-06 23:54:36 +0900

    rdoc styling

--- lib/fb_graph/connections/feed.rb
+++ lib/fb_graph/connections/feed.rb
@@ -12,27 +12,27 @@ module FbGraph
       end
 
       # == Update User Wall
-      # 
-      # me = FbGraph::User.me(ACCESS_TOKEN)
-      # me.feed!(
-      #   :message => 'Updating via FbGraph',
-      #   :picture => 'https://graph.facebook.com/matake/picture',
-      #   :link => 'http://github.com/nov/fb_graph',
-      #   :name => 'FbGraph',
-      #   :description => 'A Ruby wrapper for Facebook Graph API'
-      # )
+      #
+      #   me = FbGraph::User.me(ACCESS_TOKEN)
+      #   me.feed!(
+      #     :message => 'Updating via FbGraph',
+      #     :picture => 'https://graph.facebook.com/matake/picture',
+      #     :link => 'http://github.com/nov/fb_graph',
+      #     :name => 'FbGraph',
+      #     :description => 'A Ruby wrapper for Facebook Graph API'
+      #   )
       # 
       # == Update Page Wall
       # <em>NOTE: you need the page admin's access_token</em>
       # 
-      # page = FbGraph::Page.new(page_id, :access_token => ACCESS_TOKEN)
-      # page.feed!(
-      #   :message => 'Updating via FbGraph',
-      #   :picture => 'https://graph.facebook.com/matake/picture',
-      #   :link => 'http://github.com/nov/fb_graph',
-      #   :name => 'FbGraph',
-      #   :description => 'A Ruby wrapper for Facebook Graph API'
-      # )
+      #   page = FbGraph::Page.new(page_id, :access_token => ACCESS_TOKEN)
+      #   page.feed!(
+      #     :message => 'Updating via FbGraph',
+      #     :picture => 'https://graph.facebook.com/matake/picture',
+      #     :link => 'http://github.com/nov/fb_graph',
+      #     :name => 'FbGraph',
+      #     :description => 'A Ruby wrapper for Facebook Graph API'
+      #   )
       def feed!(options = {})
         post = post(options.merge(:connection => 'feed'))
         Post.new(post.delete(:id), options.merge(post).merge(
