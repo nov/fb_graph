c535e0d49fbac15f01daba6031322bd77a7e7b6d
Jason Byck <jasonbyck@gmail.com>
2010-12-01 17:08:19 -0500

    Venue was inaccessible from Page class.

--- lib/fb_graph/page.rb
+++ lib/fb_graph/page.rb
@@ -17,7 +17,7 @@ module FbGraph
     include Connections::Videos
     extend Searchable
 
-    attr_accessor :name, :username, :link, :category, :founded, :company_overview, :mission, :products, :fan_count, :location
+    attr_accessor :name, :username, :link, :category, :founded, :company_overview, :mission, :products, :fan_count, :location, :venue
 
     def initialize(identifier, attributes = {})
       super
