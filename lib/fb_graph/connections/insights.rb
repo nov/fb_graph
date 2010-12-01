3d21694c187ad4adc7572e731f04453bf851e92c
nov matake <nov@matake.jp>
2010-12-02 01:25:32 +0900

    get_access_token is only for FbGraph::Application, not FbGraph::Page.

--- lib/fb_graph/connections/insights.rb
+++ lib/fb_graph/connections/insights.rb
@@ -2,7 +2,8 @@ module FbGraph
   module Connections
     module Insights
       def insights(options = {})
-        options[:access_token] ||= self.access_token || get_access_token(options[:secret])
+        options[:access_token] ||= self.access_token
+        options[:access_token] ||= get_access_token(options[:secret]) if respond_to?(:get_access_token)
         insights = self.connection(:insights, options)
         insights.map! do |insight|
           Insight.new(insight.merge(:access_token => options[:access_token]))
