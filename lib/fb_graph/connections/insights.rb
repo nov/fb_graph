85e46513cdb72c6f34046ff07dda3ea4149430db
Tee Jia Hen <wizztjh@wizztjh-laptop.(none)>
2010-12-09 22:16:05 +0800

    insights

--- lib/fb_graph/connections/insights.rb
+++ lib/fb_graph/connections/insights.rb
@@ -4,7 +4,11 @@ module FbGraph
       def insights(options = {})
         options[:access_token] ||= self.access_token
         options[:access_token] ||= get_access_token(options[:secret]) if respond_to?(:get_access_token)
-        insights = self.connection(:insights, options)
+        if options[:metrics].nil?: insight_link = :insights
+        else 
+        	insight_link = "insights/options[:metrics]}"
+        end
+        insights = self.connection(insight_link, options)
         insights.map! do |insight|
           Insight.new(insight.delete(:id), insight.merge(:access_token => options[:access_token]))
         end
