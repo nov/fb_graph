836bd3f6425670b51d3c4d77bab6a9a5ad39eed5
nov matake <nov@matake.jp>
2010-12-23 22:33:38 +0900

    add connections to test_user

--- lib/fb_graph/test_user.rb
+++ lib/fb_graph/test_user.rb
@@ -1,6 +1,29 @@
 module FbGraph
   class TestUser < Node
+    include Connections::Accounts
+    include Connections::Activities
+    include Connections::Albums
+    include Connections::Books
+    include Connections::Checkins
+    include Connections::Events
+    include Connections::Feed
+    include Connections::FriendLists
     include Connections::Friends
+    include Connections::Groups
+    include Connections::Home
+    include Connections::Interests
+    include Connections::Likes
+    include Connections::Links
+    include Connections::Movies
+    include Connections::Music
+    include Connections::Notes
+    include Connections::Photos
+    include Connections::Picture
+    include Connections::Posts
+    include Connections::Statuses
+    include Connections::Tagged
+    include Connections::Television
+    include Connections::Videos
 
     attr_accessor :login_url
 
