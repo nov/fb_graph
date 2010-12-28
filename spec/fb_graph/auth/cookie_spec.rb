635bc5da3794f63ddb8c7fa44638d96d1fc6f27e
nov matake <nov@matake.jp>
2010-12-28 11:08:01 +0900

    add missing file

---
+++ spec/fb_graph/auth/cookie_spec.rb
@@ -0,0 +1,19 @@
+require File.join(File.dirname(__FILE__), '../../spec_helper')
+
+describe FbGraph::Auth::Cookie, '.parse' do
+  before do
+    @client = OAuth2::Client.new('client_id', 'client_secret')
+    @cookie = {
+      'fbs_client_id' => "access_token=t&expires=0&secret=s&session_key=k&sig=f4bae8ec88ba11440e3bdcc1bcf78317&uid=12345"
+    }
+  end
+
+  it 'should parse fbs_APP_ID cookie' do
+    cookie = FbGraph::Auth::Cookie.parse(@client, @cookie)
+    cookie[:access_token].should == 't'
+    cookie[:expires].should      == 0
+    cookie[:secret].should       == 's'
+    cookie[:session_key].should  == 'k'
+    cookie[:uid].should          == '12345'
+  end
+end
