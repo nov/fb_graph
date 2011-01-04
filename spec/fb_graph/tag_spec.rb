dd9039f297aa9db7db14438692ff459478ff1a2a
nov matake <nov@matake.jp>
2011-01-04 20:56:54 +0900

    add one more test for tag

--- spec/fb_graph/tag_spec.rb
+++ spec/fb_graph/tag_spec.rb
@@ -18,4 +18,17 @@ describe FbGraph::Tag, '.new' do
     tag.created_time.should == Time.parse("2010-04-22T08:24:26+0000")
   end
 
+  it 'should not setup user if id is blank' do
+    attributes = {
+      :id => "",
+      :name => "Arjun Banker",
+      :x => 32.5,
+      :y => 27.7778,
+      :created_time => "2010-04-22T08:24:26+0000"
+    }
+    tag = FbGraph::Tag.new(attributes)
+    tag.user.should be_nil
+    tag.name.should == 'Arjun Banker'
+  end
+
 end
