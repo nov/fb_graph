f8f62ebdd6a550768fa271081833ded03005f2f3
nov matake <nov@matake.jp>
2011-01-17 10:39:32 +0900

    version 1.3.6 add user address & mobile_email support

--- lib/fb_graph/user.rb
+++ lib/fb_graph/user.rb
@@ -33,7 +33,7 @@ module FbGraph
     # ++
     extend Searchable
 
-    attr_accessor :first_name, :last_name, :name, :link, :about, :birthday, :work, :education, :email, :website, :websites, :hometown, :location, :bio, :quotes, :gender, :interested_in, :meeting_for, :relationship_status, :religion, :political, :verified, :significant_other, :timezone, :updated_time, :sports, :favorite_teams, :favorite_athletes, :inspirational_people, :locale, :languages, :third_party_id
+    attr_accessor :first_name, :last_name, :name, :link, :about, :birthday, :work, :education, :email, :website, :websites, :hometown, :location, :bio, :quotes, :gender, :interested_in, :meeting_for, :relationship_status, :religion, :political, :verified, :significant_other, :timezone, :updated_time, :sports, :favorite_teams, :favorite_athletes, :inspirational_people, :locale, :languages, :third_party_id, :address, :mobile_phone
 
     def initialize(identifier, attributes = {})
       super
@@ -116,6 +116,10 @@ module FbGraph
       if attributes[:updated_time]
         @updated_time = Time.parse(attributes[:updated_time]).utc
       end
+      if attributes[:address]
+        @address = Venue.new(attributes[:address])
+      end
+      @mobile_phone = attributes[:mobile_phone]
     end
 
     def self.me(access_token)
