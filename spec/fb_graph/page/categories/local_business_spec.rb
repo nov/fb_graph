a23cb12dd012e210e5ff87003340c57776c9c4e4
nov matake <nov@matake.jp>
2011-03-20 00:52:19 +0900

    spec for local_business

--- spec/fb_graph/page/categories/local_business_spec.rb
+++ spec/fb_graph/page/categories/local_business_spec.rb
@@ -1,10 +1,34 @@
+require File.join(File.dirname(__FILE__), '../../../spec_helper')
+
 describe FbGraph::Page::Categories::LocalBusiness do
   before do
     fake_json(:get, 'local_business', 'pages/categories/local_business')
   end
-  subject { FbGraph::Page.new('local_business').fetch }
+  let(:page) { FbGraph::Page.new('local_business').fetch }
+  subject { page }
 
   [:attire, :culinary_team, :general_manager, :phone, :price_range, :public_transit].each do |key|
-    its(key) { should == '' }
+    its(key) { should be_instance_of String }
+  end
+
+  [:parking, :payment_options, :restaurant_services, :restaurant_specialties].each do |key|
+    its(key) { should be_instance_of Array }
+    describe key do
+      it 'should be an Array of Symbol' do
+        page.send(key).all? do |value|
+          value.should be_instance_of Symbol
+        end
+      end
+    end
+  end
+
+  its(:hours) { should be_instance_of Hash }
+  its(:hours) do
+    should == {
+      :mon => [
+        {:open  => Time.parse('Wed Mar 23 09:00:00 UTC 2011')},
+        {:close => Time.parse('Wed Mar 23 17:00:00 UTC 2011')}
+      ]
+    }
   end
 end
