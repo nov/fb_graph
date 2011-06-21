92de5604608cdceb90ad19071b2eee00187647fa
nov matake <nov@matake.jp>
2011-06-21 15:30:08 +0900

    Added - Group#docs - Group#email
    Removed
    - Group#venue (why it was there??)

--- lib/fb_graph/group.rb
+++ lib/fb_graph/group.rb
@@ -1,12 +1,13 @@
 module FbGraph
   class Group < Node
+    include Connections::Docs
     include Connections::Feed
     include Connections::Members
     include Connections::Picture
     include Connections::Videos
     extend Searchable
 
-    attr_accessor :owner, :name, :description, :link, :icon, :venue, :privacy, :version, :updated_time
+    attr_accessor :owner, :name, :email, :description, :link, :icon, :privacy, :version, :updated_time
 
     def initialize(identifier, attributes = {})
       super
@@ -14,14 +15,12 @@ module FbGraph
         @owner = User.new(owner[:id], owner)
       end
       @name         = attributes[:name]
+      @email        = attributes[:email]
       @description  = attributes[:description]
       @link         = attributes[:link]
       @icon         = attributes[:icon]
       @privacy      = attributes[:privacy]
       @version      = attributes[:version]
-      if attributes[:venue]
-        @venue = Venue.new(attributes[:venue])
-      end
       if attributes[:updated_time]
         @updated_time = Time.parse(attributes[:updated_time]).utc
       end
