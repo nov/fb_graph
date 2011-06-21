03fb30b1f243de72f87ef9f3159a14c934f35fb4
nov matake <nov@matake.jp>
2011-06-21 15:13:53 +0900

    friend list type

--- lib/fb_graph/friend_list.rb
+++ lib/fb_graph/friend_list.rb
@@ -2,11 +2,12 @@ module FbGraph
   class FriendList < Node
     include Connections::Members
 
-    attr_accessor :name
+    attr_accessor :name, :type
 
     def initialize(identifier, attributes = {})
       super
       @name = attributes[:name]
+      @type = attributes[:type]
     end
   end
 end
