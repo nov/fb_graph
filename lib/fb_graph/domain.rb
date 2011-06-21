946695e4696c0d20aa4f644569bab072533d7a3a
nov matake <nov@matake.jp>
2011-06-21 15:10:25 +0900

    one more fix for Domain

--- lib/fb_graph/domain.rb
+++ lib/fb_graph/domain.rb
@@ -11,19 +11,19 @@ module FbGraph
     #  Don't use Searchable here.
     #  Domain search doesn't return paginatable array.
     def self.search(domains)
-      fake_domain = 'facebook.com'
+      fake_domain = 'fake.com'
       domains = Array(domains)
       unless domains.include?(fake_domain)
         @using_fake = true
         domains << fake_domain
       end
       results = Node.new(nil).send(:get, :domains => domains.join(','))
-      domains.map do |identifier, attributes|
+      results.map do |identifier, attributes|
         if @using_fake && attributes[:name] == fake_domain
           next
         end
         new(identifier, attributes)
-      end
+      end.compact
     end
   end
 end
