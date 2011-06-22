d916135bcdfb6c021b78234b4e7755764247ce29
nov matake <nov@matake.jp>
2011-06-22 13:46:40 +0900

    missing files

---
+++ lib/fb_graph/connections/outbox.rb
@@ -0,0 +1,14 @@
+module FbGraph
+  module Connections
+    module Outbox
+      def outbox(options = {})
+        threads = self.connection(:outbox, options)
+        threads.map! do |thread|
+          Thread.new(thread[:id], thread.merge(
+            :access_token => options[:access_token] || self.access_token
+          ))
+        end
+      end
+    end
+  end
+end
---
+++ lib/fb_graph/connections/threads.rb
@@ -0,0 +1,14 @@
+module FbGraph
+  module Connections
+    module Threads
+      def threads(options = {})
+        threads = self.connection(:threads, options)
+        threads.map! do |thread|
+          Thread.new(thread[:id], thread.merge(
+            :access_token => options[:access_token] || self.access_token
+          ))
+        end
+      end
+    end
+  end
+end
