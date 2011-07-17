46fec8d37f73a667c46e395804596d59031f3304
nov matake <nov@matake.jp>
2011-07-17 17:19:07 +0900

    page tabs first commit

---
+++ lib/fb_graph/connections/tabs.rb
@@ -0,0 +1,14 @@
+module FbGraph
+  module Connections
+    module Tabs
+      def tabs(options = {})
+        tabs = self.connection(:tab, options)
+        tabs.map! do |tab|
+          Tab.new(tab[:id], tab.merge(
+            :access_token => options[:access_token] || self.access_token
+          ))
+        end
+      end
+    end
+  end
+end
