8322f4e25f7302b36938c80eae1dd8fc24d00c4e
Micah Wedemeyer <micah@blinqmedia.com>
2011-08-31 18:09:39 -0400

    Cleanup AdAccount
    Made more concise and removed some testing code.

--- lib/fb_graph/adaccount.rb
+++ lib/fb_graph/adaccount.rb
@@ -5,20 +5,9 @@ module FbGraph
     def initialize(identifier, attributes = {})
       super
 
-      if (users = attributes[:users])
-        @users = users.collect {|u| User.new(u[:uid], u)}
+      %w(account_id name account_status daily_spend_limit currency timezone_id timezone_name).each do |field|
+        send("#{field}=", attributes[field.to_sym])
       end
-
-      # if (owner = attributes[:owner])
-      #   @owner = User.new(owner[:id], owner)
-      # end
-      @name           = attributes[:name]
-      @account_id = attributes[:account_id]
-      @account_status = attributes[:account_status]
-      @daily_spend_limit         = attributes[:daily_spend_limit]
-      @currency        = attributes[:currency]
-      @timezone_id         = attributes[:timezone_id]
-      @timezone_name         = attributes[:timezone_name]
     end
   end
 end
