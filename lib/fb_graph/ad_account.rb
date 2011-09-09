079bb334a600c282dd9d3f8a93ff3a847728079c
Micah Wedemeyer <micah@blinqmedia.com>
2011-09-09 18:07:09 -0400

    Adds the AdCampaignStat object.
    Allows for retrieving campaign statistics at the AdAccount level.

--- lib/fb_graph/ad_account.rb
+++ lib/fb_graph/ad_account.rb
@@ -2,6 +2,7 @@ module FbGraph
   class AdAccount < Node
     include Connections::AdCampaigns
     include Connections::AdGroups
+    include Connections::AdCampaignStats
 
     attr_accessor :account_id, :name, :account_status, :daily_spend_limit, :users, :currency, :timezone_id, :timezone_name, :capabilities, :account_groups
 
