9163f22253932fadafbb6670686122aca5fe2286
nov matake <nov@matake.jp>
2011-09-21 08:25:51 +0900

    Adding these 3 methods
    FbGraph::AdUser#admin_access?
    FbGraph::AdUser#general_access?
    FbGraph::AdUser#reports_only_access?
    
    Will do the same thing for permissions.

--- lib/fb_graph/ad_user.rb
+++ lib/fb_graph/ad_user.rb
@@ -10,6 +10,17 @@ module FbGraph
       end
     end
 
+    ROLES = {
+      :admin        => 1001,
+      :general      => 1002,
+      :reports_only => 1003
+    }
+    ROLES.each do |key, value|
+      define_method "#{key}_access?" do
+        self.role <= value
+      end
+    end
+
     # FbGraph::User#fetch does not retrieve the permissions and roles since they are outside the normal
     # attributes for an FbGraph::User, so we just copy them over from this object before returning
     # the fetched one.
--- spec/fb_graph/ad_user_spec.rb
+++ spec/fb_graph/ad_user_spec.rb
@@ -1,37 +1,65 @@
 require 'spec_helper'
 
-describe FbGraph::AdUser, '.new' do
-  it 'should setup all supported attributes' do
-    attributes = {
-      :id => '123456',
+describe FbGraph::AdUser do
+  subject { ad_user }
+  let(:ad_user) { FbGraph::AdUser.new(attributes[:uid], attributes) }
+  let(:role) { FbGraph::AdUser::ROLES[:admin] }
+  let :attributes do
+    {
+      :uid => '123456',
       :permissions => [1,3,4,7],
-      :role => 1001
+      :role => role
     }
-    ad_user = FbGraph::AdUser.new(attributes.delete(:id), attributes)
+  end
+
+  it 'should setup all supported attributes' do
     ad_user.identifier.should == "123456"
     ad_user.permissions.should == [1,3,4,7]
     ad_user.role.should == 1001
   end
-end
 
-describe FbGraph::AdUser, ".fetch" do
-  let(:ad_user) do
-    FbGraph::AdUser.new(
-      "579612276",
-      :permissions => [1,3,4,7],
-      :role => 1001
-    )
+  describe 'roles' do
+    context 'when admin user' do
+      let(:role) { FbGraph::AdUser::ROLES[:admin] }
+      its(:admin_access?) { should == true }
+      its(:general_access?) { should == true }
+      its(:reports_only_access?) { should == true }
+    end
+
+    context 'when general user' do
+      let(:role) { FbGraph::AdUser::ROLES[:general] }
+      its(:admin_access?) { should == false }
+      its(:general_access?) { should == true }
+      its(:reports_only_access?) { should == true }
+    end
+
+    context 'when general user' do
+      let(:role) { FbGraph::AdUser::ROLES[:reports_only] }
+      its(:admin_access?) { should == false }
+      its(:general_access?) { should == false }
+      its(:reports_only_access?) { should == true }
+    end
   end
 
-  it "should fetch the regular User and add the AdUser attributes" do
-    mock_graph :get, '579612276', 'users/me_private', :access_token => 'access_token' do
-      fetched_user = ad_user.fetch(:access_token => 'access_token')
+  describe ".fetch" do
+    let(:ad_user) do
+      FbGraph::AdUser.new(
+        "579612276",
+        :permissions => [1,3,4,7],
+        :role => 1001
+      )
+    end
 
-      fetched_user.identifier.should == "579612276"
-      fetched_user.first_name = "Nov"
-      fetched_user.last_name = "Matake"
-      fetched_user.permissions.should == [1,3,4,7]
-      fetched_user.role.should == 1001
+    it "should fetch the regular User and add the AdUser attributes" do
+      mock_graph :get, '579612276', 'users/me_private', :access_token => 'access_token' do
+        fetched_user = ad_user.fetch(:access_token => 'access_token')
+
+        fetched_user.identifier.should == "579612276"
+        fetched_user.first_name = "Nov"
+        fetched_user.last_name = "Matake"
+        fetched_user.permissions.should == [1,3,4,7]
+        fetched_user.role.should == 1001
+      end
     end
   end
 end
