b072361c2e50bb52d1b5e7717a27b56a746bbb26
nov matake <nov@matake.jp>
2012-04-24 22:32:21 +0900

    fix for ruby 1.8

--- lib/fb_graph/comparison.rb
+++ lib/fb_graph/comparison.rb
@@ -2,7 +2,7 @@ module FbGraph
   module Comparison
     def ==(other)
       instance_variables.all? do |key|
-        if key == :@raw_attributes
+        if key.to_s == '@raw_attributes'
           :ignore_difference!
         else
           instance_variable_get(key) == other.instance_variable_get(key)
