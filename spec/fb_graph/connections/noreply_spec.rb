17876d9478799b140b620ea5a9e9db4db108ff83
nov matake <nov@matake.jp>
2012-05-10 14:49:14 +0900

    Specs for * Event#attending? * Event#maybe? * Event#declined? * Event#no_reply?
    ref #113

--- spec/fb_graph/connections/noreply_spec.rb
+++ spec/fb_graph/connections/noreply_spec.rb
@@ -1,11 +1,38 @@
 require 'spec_helper'
 
-describe FbGraph::Connections::Noreply, '#noreply' do
-  it 'should return noreply users as FbGraph::User' do
-    mock_graph :get, 'smartday/noreply', 'events/noreply/smartday_private', :access_token => 'access_token' do
-      users = FbGraph::Event.new('smartday', :access_token => 'access_token').noreply
-      users.each do |user|
-        user.should be_instance_of(FbGraph::User)
+describe FbGraph::Connections::Noreply do
+  let(:event) do
+    FbGraph::Event.new('smartday', :access_token => 'access_token')
+  end
+
+  describe '#no_reply' do
+    it 'should return noreply users as FbGraph::User' do
+      mock_graph :get, 'smartday/noreply', 'events/noreply/smartday_private', :access_token => 'access_token' do
+        event.no_reply.each do |user|
+          user.should be_instance_of(FbGraph::User)
+        end
+      end
+    end
+  end
+
+  describe '#no_reply?' do
+    context 'when no_reply' do
+      it 'should return true' do
+        mock_graph :get, 'smartday/noreply/uid', 'events/invited/not_replied', :access_token => 'access_token' do
+          event.no_reply?(
+            FbGraph::User.new('uid')
+          ).should be_true
+        end
+      end
+    end
+
+    context 'otherwise' do
+      it 'should return false' do
+        mock_graph :get, 'smartday/noreply/uid', 'empty', :access_token => 'access_token' do
+          event.no_reply?(
+            FbGraph::User.new('uid')
+          ).should be_false
+        end
       end
     end
   end
