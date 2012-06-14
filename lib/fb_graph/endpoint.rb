744bcc48945ab2f5c203b705bbbc75bb814c08c2
Kelly Martin <kellymartinv@gmail.com>
2012-06-14 15:00:03 -0500

    access_token has to be on the relative_url, not in the body

--- lib/fb_graph/endpoint.rb
+++ lib/fb_graph/endpoint.rb
@@ -1,10 +1,11 @@
 module FbGraph
   class Endpoint < Array
-    def initialize(relative_url)
+    def initialize(relative_url, params = {})
       @relative_url = relative_url || []
+      @params = params
     end
     def relative_url
-      File.join @relative_url
+      "#{File.join(@relative_url).to_s}#{@params[:access_token] ? "?access_token=#{@params[:access_token]}" : ''}"
     end
     def to_s
       File.join [FbGraph::ROOT_URL, @relative_url]
