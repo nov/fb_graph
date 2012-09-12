6fd945380552a5645c6ee7ff1a52b24de1b4af4e
Kris Kemper and Syl Turner <dev+kris+syl@blinqmedia.com>
2012-09-12 14:47:55 -0400

    Added remaining creative attributes provided by facebook to AdCreative

--- spec/fb_graph/ad_creative_spec.rb
+++ spec/fb_graph/ad_creative_spec.rb
@@ -17,7 +17,13 @@ describe FbGraph::AdCreative, '.new' do
       :run_status => 1,
       :preview_url => "http://www.facebook.com/ads/api/creative_preview.php?cid=6003590469668",
       :count_current_adgroups => 1,
-      :image_url => "https://www.google.com/image.png"
+      :object_id => 12345,
+      :story_id => 54321,
+      :image_url => "https://www.google.com/image.png",
+      :url_tags => "url=tags",
+      :related_fan_page => 5799040003,
+      :auto_update => 1,
+      :action_spec => '{"action.type":["flightsim:fly"], "application":[174829001234]}'
     }
     ad_creative = FbGraph::AdCreative.new(attributes.delete(:id), attributes)
     ad_creative.identifier.should == "6003590469668"
@@ -33,7 +39,12 @@ describe FbGraph::AdCreative, '.new' do
     ad_creative.run_status.should == 1
     ad_creative.preview_url.should == "http://www.facebook.com/ads/api/creative_preview.php?cid=6003590469668"
     ad_creative.count_current_adgroups.should == 1
+    ad_creative.facebook_object_id.should == 12345
     ad_creative.image_url.should == "https://www.google.com/image.png"
+    ad_creative.url_tags.should == "url=tags"
+    ad_creative.related_fan_page.should == 5799040003
+    ad_creative.auto_update.should == 1
+    ad_creative.action_spec.should == '{"action.type":["flightsim:fly"], "application":[174829001234]}'
   end
 end
 
