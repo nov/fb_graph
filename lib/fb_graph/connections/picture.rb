56d65a1f13ed17a437fb70056b885b1d5d63d903
Andrew Korzhuev <wolfon@gmail.com>
2012-10-30 20:00:20 +0400

    Support of width and height params to picture size now supported by facebook

--- lib/fb_graph/connections/picture.rb
+++ lib/fb_graph/connections/picture.rb
@@ -1,12 +1,23 @@
 module FbGraph
   module Connections
     module Picture
-      def picture(size = nil)
-        _endpoint_ = "#{self.endpoint}/picture"
-        if size
-          _endpoint_ += "?type=#{size}"
+      # User can specify size in two ways: 
+      # 1. By type: square | small | normal | large
+      # 2. By width and height, so facebook return the _closest_
+      #    match to the size you specified, if only one parameter
+      #    is set, then facebook will return square image
+      # See: https://developers.facebook.com/docs/reference/api/user/
+      def picture(size_or_width = nil, height = nil)        
+        params = {}
+        if size_or_width.kind_of? String or size_or_width.kind_of? Symbol
+          params[:type]   = size_or_width
+        elsif size_or_width.kind_of? Fixnum or not height.nil?
+          params[:width]  = size_or_width  unless size_or_width.nil?
+          params[:height] = height         unless height.nil?
         end
-        _endpoint_
+        params = params.empty? ? '' : "?#{URI.encode_www_form(params)}"
+
+        "#{self.endpoint}/picture#{params}"
       end
     end
   end
--- spec/fb_graph/connections/picture_spec.rb
+++ spec/fb_graph/connections/picture_spec.rb
@@ -8,9 +8,21 @@ describe FbGraph::Connections::Picture, '#picture' do
     end
 
     it 'should support size option' do
-      [:square, :large].each do |size|
+      [:square, :small, :normal, :large].each do |size|
         FbGraph::User.new('matake').picture(size).should == File.join(FbGraph::ROOT_URL, "matake/picture?type=#{size}")
       end
+    end
+
+    it 'should support width option' do
+      FbGraph::User.new('matake').picture(13).should == File.join(FbGraph::ROOT_URL, "matake/picture?width=13")
+    end
+
+    it 'should support height option' do
+      FbGraph::User.new('matake').picture(nil, 37).should == File.join(FbGraph::ROOT_URL, "matake/picture?height=37")
+    end
+
+    it 'should support width and height options at the same time' do
+      FbGraph::User.new('matake').picture(13, 37).should == File.join(FbGraph::ROOT_URL, "matake/picture?width=13&height=37")
     end
   end
 
@@ -20,10 +32,22 @@ describe FbGraph::Connections::Picture, '#picture' do
     end
 
     it 'should support size option' do
-      [:square, :large].each do |size|
+      [:square, :small, :normal, :large].each do |size|
         FbGraph::Page.new('platform').picture(size).should == File.join(FbGraph::ROOT_URL, "platform/picture?type=#{size}")
       end
     end
+
+    it 'should support width option' do
+      FbGraph::Page.new('platform').picture(13).should == File.join(FbGraph::ROOT_URL, "platform/picture?width=13")
+    end
+
+    it 'should support height option' do
+      FbGraph::Page.new('platform').picture(nil, 37).should == File.join(FbGraph::ROOT_URL, "platform/picture?height=37")
+    end
+
+    it 'should support width and height options at the same time' do
+      FbGraph::Page.new('platform').picture(13, 37).should == File.join(FbGraph::ROOT_URL, "platform/picture?width=13&height=37")
+    end    
   end
 
 end
