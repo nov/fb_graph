d9d8c20fecae76cac0beb0383d9120aff13802ee
eric.vautour <eric.vautour@kineticsocial.com>
2012-12-05 11:18:44 -0500

    Local Changes included

--- lib/fb_graph/batch_request.rb
+++ lib/fb_graph/batch_request.rb
@@ -1,13 +1,14 @@
 module FbGraph
   # Batch Request
   class BatchRequest < BatchBase
-    attr_accessor :objects, :errors
+    attr_accessor :objects, :errors, :root_errors
     
     # Initialize object and run request
     def initialize(access_token, facebook_ids, options = {})
       super(access_token, options)
       self.objects = []
       self.errors = []
+      self.root_errors = []
       timeout_and_retry { request(facebook_ids) } 
     end
     
@@ -20,6 +21,9 @@ module FbGraph
       params = {:batch => facebook_ids.collect{|id| {:relative_url => id.to_s}}}
       
       begin
+        # Was there nils returned in the request
+        nil_returns = false
+       
         # We would like to keep requests open in development mode, but disable during testing
         return if disable_facebook_interaction? && Rails.env.test?
         
@@ -33,18 +37,32 @@ module FbGraph
         handle_response {response} if body.kind_of?(Hash)
         
         # Gather all the body objects
-        body_collection = body.collect {|r| r["body"]}
+        body_collection = body.inject([]) do |a, r|
+          # Sometimes Facebook returns nil as a body, there is no additional infomation as to why so we will just skip it
+          (r.blank?) ? nil_returns = true : a << r["body"]
+          a
+        end
         
         # Decode each json body, and create the objects array
         body_collection.each do |result|
           next if result == "false"
           o = ActiveSupport::JSON.decode(result).with_indifferent_access
-          objects << o if o[:error].blank? # If couldn't be found, then error object is returned do not include it
+          objects << o if o[:error].blank? # If couldn't be found, then error object is returned do not include it          
+        end
+
+        # Did we miss any
+        diff = facebook_ids.collect{|id| id.to_i} - objects.collect{|o| o[:id].to_i} rescue []
+        
+        # If we had nil returns, then we want to raise exceptions to retry
+        # If we've exausted our retry attempts, then we want to send a message 
+        # to the root users only and not raise an exception
+        if nil_returns
+          raise Exception.new(400, "Missing ID(s) Going to retry") if retries < RETRY_COUNT
+          root_errors << "NIL Body Returned for object(s) on attempt #{retries}\n\n Request: #{params}\n\nMISSING: #{diff}"
         end
         
-        # Did we miss any, if it matters
-        unless skip_missing_id_check
-          diff = facebook_ids.collect{|id| id.to_i} - objects.collect{|o| o[:id].to_i}
+        # Send errors if it matters
+        unless skip_missing_id_check          
           errors << "The following ID(s) were not found in Facebook #{diff.join(',')}" unless diff.blank?
         end
       rescue *[TypeError, NoMethodError] => e
