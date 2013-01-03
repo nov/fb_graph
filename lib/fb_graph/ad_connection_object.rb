1a6f0ba08f809e9e0bc2e4c212ab41b82f975d47
eric.vautour <eric.vautour@kineticsocial.com>
2013-01-03 13:42:02 -0500

    TASK KSP-513: Added og_actions and og_namespace to connection objects

--- lib/fb_graph/ad_connection_object.rb
+++ lib/fb_graph/ad_connection_object.rb
@@ -1,6 +1,6 @@
 module FbGraph
   class AdConnectionObject < Node
-    attr_accessor :name, :url, :type, :tabs, :picture, :object
+    attr_accessor :name, :url, :type, :tabs, :picture, :object, :is_game, :og_actions, :og_namespace, :og_objects, :supported_platforms
 
     TYPES = {
       :page => 1,
@@ -9,12 +9,27 @@ module FbGraph
       :place => 6,
       :domain => 7
     }
-
+    
+    SUPPORTED_PLATFORM_TYPES = {
+      :web => 1,
+      :canvas => 2,
+      :mobile_web => 3,
+      :iphone => 4,
+      :ipad => 5,
+      :android => 6
+    }
+    
     def initialize(identifier, attributes = {})
       super
 
       %w(name url type tabs picture).each do |field|
         send("#{field}=", attributes[field.to_sym])
+      end
+      
+      if self.type == TYPES[:application]
+        %w(is_game og_actions og_namespace og_objects supported_platforms).each do |field|
+          send("#{field}=", attributes[field.to_sym])
+        end
       end
 
       self.object = if page?
