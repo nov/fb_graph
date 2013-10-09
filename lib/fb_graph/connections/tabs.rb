c46f7267e317f89adf64da6c87b536f8c85bd7e4
David Underwood <david.underwood@jadedpixel.com>
2013-10-08 22:01:53 -0400

    Adds tab_for_app method to tabs connection

--- lib/fb_graph/connections/tabs.rb
+++ lib/fb_graph/connections/tabs.rb
@@ -18,6 +18,14 @@ module FbGraph
         tab = self.connection :tabs, options.merge(:connection_scope => application.identifier)
         tab.present?
       end
+
+      def tab_for_app(application, options = {})
+        response = get options.merge(:connection => :tabs, :connection_scope => application.identifier)
+        tab = response[:data].first
+        Tab.new tab[:id], tab.merge(
+          :access_token => options[:access_token] || self.access_token
+        )
+      end
     end
   end
 end
