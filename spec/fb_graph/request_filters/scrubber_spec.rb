d57fb714494eb68480c5a05aaa355e0bd1d52901
nov <nov@matake.jp>
2013-12-27 04:34:02 +0900

    ok 1.9.3, you are not in UTF world.

--- spec/fb_graph/request_filters/scrubber_spec.rb
+++ spec/fb_graph/request_filters/scrubber_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe FbGraph::RequestFilters::Scrubber do
   describe '#filter_response' do
+    let(:original_body) { "{ \"name\": \"John\x80\" }".force_encoding('UTF-8') }
     let(:resource_endpoint) { 'https://graph.facebook.com/matake' }
     let(:request) { HTTP::Message.new_request(:get, URI.parse(resource_endpoint)) }
-    let(:response) { HTTP::Message.new_response("{ \"name\": \"John\x80\" }".force_encoding('UTF-8')) }
+    let(:response) { HTTP::Message.new_response(original_body) }
     let(:request_filter) { FbGraph::RequestFilters::Scrubber.new }
 
     if 'string'.respond_to?(:scrub!)
@@ -15,7 +16,7 @@ describe FbGraph::RequestFilters::Scrubber do
     else
       it 'should not do anything' do
         request_filter.filter_response(request, response)
-        response.body.should eq "{ \"name\": \"John\x80\" }"
+        response.body.should eq original_body
       end
     end
   end
